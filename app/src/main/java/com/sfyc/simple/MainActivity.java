bc454a38eec445c169ffa11656d39ca9a8301b02
sfyc23 <sfyc23@gmail.com>
2016-12-20 19:28:31 +0800

    Initial commit

---
+++ app/src/main/java/com/sfyc/simple/MainActivity.java
@@ -0,0 +1,38 @@
+package com.sfyc.simple;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.sfyc.ctpv.CountTimeProgressView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private CountTimeProgressView countTimeProgressView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+        countTimeProgressView = (CountTimeProgressView) findViewById(R.id.countTimeProgressView);
+        countTimeProgressView.addOnEndListener(new CountTimeProgressView.OnEndListener() {
+            @Override
+            public void onAnimationEnd() {
+                startActivity(new Intent(MainActivity.this,SimpleActivity.class));
+                finish();
+            }
+
+            @Override
+            public void onClick(long overageTime) {
+                startActivity(new Intent(MainActivity.this,SimpleActivity.class));
+                finish();
+            }
+
+        });
+        countTimeProgressView.startCountTimeAnimation();
+    }
+}
